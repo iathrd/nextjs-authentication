c4aea2e707170f101a8470b11e7f479f92eeae73
Iqbal Athorid <iqbalathoridd@gmail.com>
2024-03-21 13:29:12 +0700

    create login card ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,36 @@
-import Image from "next/image";
+import { Poppins } from "next/font/google";
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import LoginButtoon from "@/components/auth/login-button";
+
+const font = Poppins({
+  subsets: ["latin"],
+  weight: ["600"],
+});
 
 export default function Home() {
-  return <p className="font-semibold text-green-500">Hello World</p>;
+  return (
+    <main className="flex h-full flex-col items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-800">
+      <div className="space-y-6">
+        <h1
+          className={cn(
+            "text-6xl font-semibold text-white drop-shadow-md",
+            font.className
+          )}
+        >
+          Auth
+        </h1>
+        <p className="text-white text-lg">
+          A simple authentication app using Next.js
+        </p>
+        <div>
+          <LoginButtoon>
+            <Button variant="secondary" size="lg">
+              Sign in
+            </Button>
+          </LoginButtoon>
+        </div>
+      </div>
+    </main>
+  );
 }
